8df21024e8c51b2abc843e28e8c18801ef4bcb37
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-11-16 23:01:47 +0600

    new message on post method checked on postman

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 // Importing
 import express from 'express';
+import mongoose from 'mongoose';
+import Messages from './dbMessages.js';
 
 // App config
 const app = express();
 const port = process.env.PORT || 4200
 
 // Middleware
+app.use(express.json())
 
 // DB config
+const connection_uri = `mongodb+srv://admin:b7WJkmEQMZDv5TcO@cluster0.qlvoh.mongodb.net/whatsupDb?retryWrites=true&w=majority`
+
+mongoose.connect(connection_uri, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 // API routes
 app.get('/', (req, res) => {
     res.status(200).send('hello programmer')
 })
+
+app.post('/messages/new', (req, res) => {
+    const dbMessage = req.body
+
+    Messages.create(dbMessage, (err, data) => {
+        if(err) res.status(500).send(err)
+        else res.status(201).send(`New message created: \n ${data}`)
+    })
+})
+
 
 // Listener
 app.listen(port, () => console.log(`Listening to the local host : ${port}`))
