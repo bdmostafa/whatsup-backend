6d3abcc52fa2d746919f8bd193531774bb8dcfb4
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-11-16 23:10:01 +0600

    get all the mesages checked on postman

--- server.js
+++ server.js
@@ -24,6 +24,13 @@ app.get('/', (req, res) => {
     res.status(200).send('hello programmer')
 })
 
+app.get('/messages/sync', (req, res) => {
+    Messages.find((err, data) => {
+        if(err) res.status(500).send(err)
+        else res.status(200).send(data)
+    })
+})
+
 app.post('/messages/new', (req, res) => {
     const dbMessage = req.body
 
